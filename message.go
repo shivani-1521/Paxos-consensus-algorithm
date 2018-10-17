1b58a874c879e5e1ed5e714ab5eca352380a802c
shivani <f2016680@pilani.bits-pilani.ac.in>
2018-10-18 01:06:12 +0530

    basic functionality

---
+++ message.go
@@ -0,0 +1,41 @@
+package paxos
+
+type msgType int
+
+const(
+	Prepare msgType = iota + 1
+	Propose
+	Promise
+	Accept
+)
+
+type message struct{
+	from			int
+	to				int
+	type    		msgType
+	sequence		int
+	preSequence		int
+	value				string
+
+}
+
+func (m *message)getSeqNumber() int{
+	return m.sequence
+}
+
+func (m *message)getProposeValue() string{
+	return m.value
+}
+
+//if
+
+func (m *message)getProposeSeq() int{
+	switch m.typ {
+	case Promise:
+		return m.preSequence
+	case Accept:
+		return m.sequence
+	default:
+		panic("message type not supported")
+	}
+}
